e7776ecbdb515cef3561b7c8986a1b47f3d1b0a2
Santosh Kotagi <DevOpsPOC@hotmail.com>
2021-04-29 19:15:09 +0530

    changed cluster name

--- terrafrom-manifests/aks-administration-azure-ad.tf
+++ terrafrom-manifests/aks-administration-azure-ad.tf
@@ -1,6 +1,6 @@
 # Create Azure AD group in Active deirectory for AKS admins
 resource "azuread_group" "aks_administrators" {
-    name = "${azurerm_resource_group.aks_rg.name}-cluster-administrators"
+    name = "${azurerm_resource_group.aks_rg.name}-administrators"
     description = "Azure AKS Kubernetes administrators for the $(resource_group_name.aks.rg.name)-cluster"
   
 }
